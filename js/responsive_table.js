7881ba8674431ca32a131e21e2c5c9f86525c957
Trent Stromkins <trent@smallrobot.org>
2013-05-30 15:32:51 -0700

    Update Annotations

--- js/responsive_table.js
+++ js/responsive_table.js
@@ -2,9 +2,8 @@
   Drupal.behaviors.views_mediatable = {
     attach: function (context, settings) {
 
-      //code starts
+      // Enables the plugin on tables with the class ".responsive-table".
       $('table.responsive-table').mediaTable();
-      //code ends
 
     }
   };
